aa3a1558143881a9b34b0f9d404c3c25fab68fa8
Daniele <dgermani@everis.com>
2019-06-27 16:26:17 +0200

    dgermani: added data picker compontnt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatButtonModule, MatSelectModule, MatIconModule, MatMenuModule, MatProgressBarModule, MatRadioModule, MatSliderModule, MatCardModule } from '@angular/material';
+import { MatInputModule, MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatSelectModule, MatIconModule, MatMenuModule, MatProgressBarModule, MatRadioModule, MatSliderModule, MatCardModule } from '@angular/material';
 import { FormsModule } from '@angular/forms';
 import { CardComponent } from './card/card.component';
+import { DataPickerComponent } from './data-picker/data-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    DataPickerComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +28,9 @@ import { CardComponent } from './card/card.component';
     MatProgressBarModule,
     MatRadioModule,
     MatSliderModule,
-    MatCardModule
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
